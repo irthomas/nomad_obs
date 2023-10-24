a7c9fd8a50a009a7c5c7794f255c058d091651e5
irthomas <ithomas84@gmail.com>
2023-10-24 15:16:43 +0200

    MTP074 done

--- nomad_obs/check_cop_rows_in_summary_files.py
+++ nomad_obs/check_cop_rows_in_summary_files.py
@@ -17,7 +17,7 @@ import os
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
-mtpNumber = 73
+mtpNumber = 74
 
 #add the correct MTP info in obs_inputs
 from nomad_obs.mtp_inputs import getMtpConstants
