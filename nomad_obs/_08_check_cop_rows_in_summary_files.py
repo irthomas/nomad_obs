eed891f6a2e9cbd1b2335c6a2f94c08fe94114ca
irthomas <ithomas84@gmail.com>
2026-09-04 09:31:04 +0200

    MTP111 done; MTP112 overview files downloaded

--- nomad_obs/_08_check_cop_rows_in_summary_files.py
+++ nomad_obs/_08_check_cop_rows_in_summary_files.py
@@ -20,7 +20,7 @@ from datetime import datetime, timedelta
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
-mtpNumber = 110
+mtpNumber = 111
 # add the correct MTP info in obs_inputs
 
 DT_STR = "%Y %b %d %H:%M:%S"
